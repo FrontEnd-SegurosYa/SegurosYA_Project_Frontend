79d5660635a6868c56ce3db556e52d9c77e68d74
Valerie0607 <89219676+Valerie0607@users.noreply.github.com>
2023-05-03 00:28:11 -0500

    act 4
    actualizacion con imagenes y fechas

--- segurosya_frontend/src/componentes/navbar/NavbarElements.js
+++ segurosya_frontend/src/componentes/navbar/NavbarElements.js
@@ -1,18 +1,20 @@
 import './Navbar.css'
+import logo from '../../img/logoNombre.png';
+import telefono from '../../img/telefonoGris.png';
 
 
 const NavbarElements = () => {
   return(
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src = "../../img/logoNombre.png" alt="" width="30" height="24"/>
+        <a class="navbar-brand logo mx-2" href="#">
+          <img src = {logo} alt="" width="80" height="40" class="d-inline-block align-text-top"/>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-5">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-3">
             <li class="nav-item dropdown">
             <a class="nav-link active dropdown-toggle btn-lg tituloNavbar" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Sobre Seguros
@@ -29,8 +31,8 @@ const NavbarElements = () => {
             </li>
           </ul>
           <div class = "telefono">
-            <img src="./../../img/telefonoGris.png" class="img-fluid"></img>
-            <span class="navbar-brand mb-0 tituloNavbar mx-3"> (01) 654 3636</span>
+            <img src={telefono} class="img-fluid" width="17" height="17"></img>
+            <span class="navbar-brand mb-0 tituloNavbar mx-2"> (01) 654 3636</span>
           </div>
           <form class="d-flex mx-3 ">
             <button type="button" class="btnGeneral">Ingresa a tu Cuenta</button>
