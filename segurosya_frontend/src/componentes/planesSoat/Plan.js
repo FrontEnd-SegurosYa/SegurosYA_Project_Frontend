65ccf2d8e4bed4f70236efc7619c3a55618b9afd
sunjis <sun.ji@pucp.edu.pe>
2023-05-30 10:12:00 -0500

    Actualización de PlanesSOAT
    Cambio en el hover de la selección de planes SOAT

--- segurosya_frontend/src/componentes/planesSoat/Plan.js
+++ segurosya_frontend/src/componentes/planesSoat/Plan.js
@@ -1,27 +1,42 @@
-import React , { useState }from 'react';
-import './Plan.css'; // Importa el archivo CSS para estilizar la tarjeta
+import React, { useState } from 'react';
+import './Plan.css';
 
-const Plan = ({id, selected, onClick, title, pago, costo, coberturas, asistencias, beneficios, image }) => {
-    return (
-        <div className={`plan ${selected ? 'clicked' : ''}`} onClick={onClick}>
+const Plan = ({ id, selected, onClick, title, pago, costo, coberturas, asistencias, beneficios, image }) => {
+  const [hovered, setHovered] = useState(false);
+  //cambiar el mouse en el hover
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  return (
+        <div
+        className={`plan ${selected ? 'clicked' : ''} ${hovered ? 'hovered' : ''}`}
+        onClick={onClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        >
           <img src={image} alt={title} />
           <div className="plan-content">
-            <h2 className="nombre"><b>{title}</b></h2>
+            <h2 class="nombre"><b>{title}</b></h2>
             <h6><b>{pago}</b></h6>
-            <h2 className="azulos"><b>{costo}</b></h2>
-            <p className="azulos"><b>Coberturas</b></p>
+            <h2 class="azulos"><b>{costo}</b></h2>
+            <p class="azulos"><b>Coberturas</b></p>
             <ul>
                 {coberturas.map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
-            <p className="azulos"><b>Asistencias</b></p>
+            <p class="azulos"><b>Asistencias</b></p>
             <ul>
                 {asistencias.map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
-            <p className="azulos"><b>Beneficios</b></p>
+            <p class="azulos"><b>Beneficios</b></p>
             <ul>
                 {beneficios.map((item, index) => (
                     <li key={index}>{item}</li>
@@ -33,3 +48,4 @@ const Plan = ({id, selected, onClick, title, pago, costo, coberturas, asistencia
 };
 
 export default Plan;
+
