f340e3a9ed2e61ff665e2c029506bf866a685e3e
MarceloGal <69372076+MarceloGal@users.noreply.github.com>
2023-06-22 19:42:53 -0500

    nuevo link

--- segurosya_frontend/src/utiles/constantes.js
+++ segurosya_frontend/src/utiles/constantes.js
@@ -1,4 +1,5 @@
 module.exports = {
-    LINKSERVER: "http://3.89.34.248:8080",
+    LINKSERVER: "http://44.198.73.103:8080",
+    // LINKSERVER: "http://3.89.34.248:8080",
     // LINKSERVER: "http://localhost:8080",
 };
