67123980a06bafae7ab2252191798ba5424ba88e
MarceloGal <69372076+MarceloGal@users.noreply.github.com>
2023-06-23 13:47:29 -0500

    Eliminado node modules y otros

--- segurosya_frontend/src/utiles/constantes.js
+++ segurosya_frontend/src/utiles/constantes.js
@@ -1,5 +1,7 @@
 module.exports = {
-    LINKSERVER: "http://44.198.73.103:8080",
+    // EC2 Bruno
+	LINKSERVER: "http://44.198.73.103:8080",
+	// EC2 Lennon
     // LINKSERVER: "http://3.89.34.248:8080",
     // LINKSERVER: "http://localhost:8080",
 };
