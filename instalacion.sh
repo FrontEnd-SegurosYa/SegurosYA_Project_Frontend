885d29f51dbe72d2b149b14d3cfd15c284762665
MarceloGal <69372076+MarceloGal@users.noreply.github.com>
2023-07-09 18:28:23 -0500

    Nueva version de script de instalacion
    Probado al 90% de funcionamiento.

--- instalacion.sh
+++ instalacion.sh
@@ -23,15 +23,44 @@ fi
 
 
 
-#Lectura de argumentos
-source argumentos.txt
+# Actualizar los repositorios
+sudo apt-get update
 
+# Instalar editor de JSON por comando
+sudo apt-get install -y jq
+
+# Instalar Apache2
+sudo apt-get install -y apache2
+
+# Instalar Node.js y npm
+sudo apt-get install -y nodejs npm
+
+# Cierre temporal del servidor Apache
+sudo service apache2 stop
+
+# Obtencion de linkFrontEnd y linkBackEnd
+source "./argumentos.txt"
+echo $linkFrontEnd
+echo $linkBackEnd
+
+# Ubicacion de package.json
+package_file="./segurosya_frontend/package.json"
+
+# Nuevo valor para la propiedad "homepage"
+new_homepage="https://$linkFrontEnd"
+
+# Modificar el valor de la propiedad "homepage" en el archivo package.json
+jq --arg new_homepage "$new_homepage" '.homepage = $new_homepage' "$package_file" > temp.json && mv temp.json "$package_file"
+
+# Modificacion de constante de servidor de FRONT END
 contenidoLinkBackEnd="
 module.exports = {
     LINKSERVER: \"$linkBackEnd:8080\",
 };
 "
+echo "$contenidoLinkBackEnd" > ./segurosya_frontend/src/utiles/constantes.js
 
+# Creacion de archivo SegurosYa.conf
 contenidoVirtualConf="
 <VirtualHost *:80>
 	ServerName $linkFrontEnd
@@ -40,7 +69,6 @@ contenidoVirtualConf="
 	DocumentRoot /var/www/html
 
 	<Directory /var/www/html>
-		
 		Options -Indexes +FollowSymLinks +MultiViews
 		AllowOverride All
 		Require all granted
@@ -54,59 +82,27 @@ contenidoVirtualConf="
 	</Directory>
 
 	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-	
+	CustomLog ${APACHE_LOG_DIR}/access.log combined	
 </VirtualHost>
 "
-
-# Actualizar los repositorios
-sudo apt-get update
-
-# Instalar editor de JSON por comando
-sudo apt-get install -y jq
-
-# Instalar Apache2
-sudo apt-get install -y apache2
-
-# Instalar Node.js y npm
-sudo apt-get install -y nodejs npm
-
-# Actualizar npm a la última versión
-sudo npm install -g npm
-
-# Cierre temporal del servidor Apache
-sudo service apache2 stop
-
-# Ubicacion de package.json
-package_file="segurosya_frontend/package.json"
-
-# Nuevo valor para la propiedad "homepage"
-new_homepage="https://$linkFrontEnd"
-
-# Modificar el valor de la propiedad "homepage" en el archivo package.json
-jq --arg new_homepage "$new_homepage" '.homepage = $new_homepage' "$package_file" > temp.json && mv temp.json "$package_file"
-
-# Creacion de archivos para servidor
-echo "$contenidoLinkBackEnd" > "segurosya_frontend/src/utiles/constantes.js"
-echo "$contenidoVirtualConf" > "/etc/apache2/sites-available/SegurosYa.conf"
+echo "$contenidoVirtualConf" > ./SegurosYa.conf
 
 # Creacion de archivos estaticos en React
 cd segurosya_frontend/
 npm ci && npm run build
 sudo rm -rf /var/www/html/* && sudo cp -r build/* /var/www/html/
+cd ..
 
-# Configuracion de servidor Apache
-sudo a2enmod rewrite
-sudo ln -s /etc/apache2/sites-available/SegurosYa.conf /etc/apache2/sites-enabled/
+# Habilitacion del archivo .conf
+sudo rm -f /etc/apache2/sites-available/SegurosYa.conf
+sudo cp ./SegurosYa.conf /etc/apache2/sites-available/
+
+sudo a2ensite SegurosYa.conf
+
+# Reinicio de apache2
 sudo service apache2 restart
 
-echo "Gracias por instalar el servidor Front-End de Clientes de SegurosYa."
-echo "Puede acceder al sitio a traves de: http://$linkFrontEnd"
-
-
-
-
-
-
-
+# Mensaje de exito
+echo "Gracias por instalar el servidor Front-End de cliente de Seguros Ya."
+echo "El siguiente link le servira para acceder a la pagina web:"
+echo "https://$linkFrontEnd"
